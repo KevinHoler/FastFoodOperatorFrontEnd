a706ffd64e636853d378063455dd908c886cb7c7
KevinHoler <holer.kevin@gmail.com>
2025-04-03 10:58:11 +0200

    made buttons in header to display the components

--- FrontendFastFoodOperator/src/App.vue
+++ FrontendFastFoodOperator/src/App.vue
@@ -4,20 +4,36 @@ import PizzaList from './components/PizzaList.vue';
 import { ref } from 'vue';
 
 const cart = ref([]);
+const showPizzaList = ref(false);
+const showCart = ref(false);
+
+const togglePizzaList = () => {
+  showPizzaList.value = !showPizzaList.value;
+  showCart.value = false; 
+};
+
+const toggleCart = () => {
+  showCart.value = !showCart.value;
+  showPizzaList.value = false; 
+};
 </script>
 
 <template>
   <header>
     <img alt="FFO logo" class="logo" src="./assets/ffo.png" width="125" height="125" />
-    <div>
+    <div class="header-content">
       <h1>Welcome to Fast Food Operator</h1>
+      <div class="button-container">
+        <button @click="togglePizzaList">Menu</button>
+        <button @click="toggleCart">Cart</button>
+      </div>
     </div>
   </header>
 
   <main>
     <div class="content">
-      <PizzaList @add-to-cart="cart.push($event)" />
-      <Cart :cart="cart" @remove="cart.splice($event, 1)" />
+      <PizzaList v-if="showPizzaList" @add-to-cart="cart.push($event)" />
+      <Cart v-if="showCart" :cart="cart" @remove="cart.splice($event, 1)" />
     </div>
   </main>
 </template>
@@ -44,6 +60,34 @@ header {
   justify-content: space-between;
   padding: 10px;
   color: rgb(18, 17, 17);
+}
+
+.header-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+header button {
+  flex: 1;
+  margin: 10px;
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  cursor: pointer;
+  height: 40px; 
+}
+
+header button:hover {
+  background-color: #45a049;
 }
 
 main {
