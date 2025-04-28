7c10a3036bdc5b92d9be7ff98977bf1ee6f2018c
zully76 <zuleima.alexandra.martinez@hotmail.com>
2025-04-28 12:32:55 +0200

    Fixed object syntax in createOrder to include totalPrice

--- FrontendFastFoodOperator/src/components/CartComponent.vue
+++ FrontendFastFoodOperator/src/components/CartComponent.vue
@@ -49,7 +49,8 @@ export default {
         pizzaIds,
         drinkIds,
         extraIds,
-        EatHere: eatHere
+        EatHere: eatHere,
+        totalPrice: this.totalPrice
       };
 
       try {
