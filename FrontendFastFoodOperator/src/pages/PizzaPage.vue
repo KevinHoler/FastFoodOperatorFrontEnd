ac34d0c5c736ac3eaa649740fc5a48b949b83d06
zully76 <zuleima.alexandra.martinez@hotmail.com>
2025-04-29 16:35:53 +0200

    feat: Lägg till funktionalitet för att hantera toppings på beställningar
    Denna commit introducerar funktionalitet för att lägga till och hantera toppings på beställningar. Följande ändringar har gjorts:
    
    - Skapade en ny komponent för att visa och välja toppings.
    - Implementerade logiken för att lägga till valda toppings till beställningsobjektet.
    - Uppdaterade gränssnittet för att visa de valda toppings på beställningen.
    
    Obs: På grund av problem med min utvecklingsmiljö har jag inte kunnat testa detta visuellt fullständigt. Vänligen granska och testa noggrant i er egen miljö.

--- FrontendFastFoodOperator/src/pages/PizzaPage.vue
+++ FrontendFastFoodOperator/src/pages/PizzaPage.vue
@@ -5,24 +5,78 @@
     </div>
         <NavComponent/>
         <PizzaList @add-to-cart="addToCart" />
+
+>
+<div v-if="pizza">
+    <h2>Ingredienser:</h2>
+    <ul>
+      <li v-for="ingredient in pizza.ingredients" :key="ingredient.id">
+        <input 
+          type="checkbox" 
+          :value="ingredient.id" 
+          v-model="selectedIngredients" 
+          :id="'ingredient-' + ingredient.id" 
+        />
+        <label :for="'ingredient-' + ingredient.id">{{ ingredient.name }} ({{ ingredient.price }}kr)</label>
+      </li>
+    </ul>
+    <button @click="addToppingsToPizza">Lägg till toppingar</button>
+  </div>
 </template>
 
 
 <script>
 import NavComponent from '@/components/NavComponent.vue';
 import PizzaList from '@/components/PizzaList.vue';
+import axios from 'axios';
 
 
 export default {
+
     name: 'PizzaPage',
     props: {
     cart: Array
   },
+
   components: {
     PizzaList,
     NavComponent
   },
+  data() {
+    return {
+      pizza: null, // Pizza seleccionada
+      selectedIngredients: [] // Ingredientes seleccionados por el usuario para toppings
+    };
+  },
+  created() {
+    this.fetchPizzaDetails();
+  },
   methods: {
+    
+    async fetchPizzaDetails() {
+      const pizzaId = this.$route.params.id;
+      try {
+        const response = await axios.get(`http://localhost:5062/pizza/${pizzaId}`);
+        this.pizza = response.data;
+      } catch (error) {
+        console.error('Error fetching pizza details:', error);
+      }
+    },
+    async addToppingsToPizza() {
+      const pizzaId = this.pizza.id;
+      try {
+        
+        const response = await axios.put(`http://localhost:5062/orders/1/pizzas/${pizzaId}/toppings`, {
+          toppingIds: this.selectedIngredients
+        });
+        alert('Toppingar tillagda!');  
+      } catch (error) {
+        alert('Det gick inte att lägga till toppingar.');   
+      }
+    },
+  
+  
+  
     addToCart(pizza) {
       this.cart.push(pizza);
     },
@@ -31,6 +85,7 @@ export default {
     }
   }
 }
+
 </script>
 <style>
 .top-container{
